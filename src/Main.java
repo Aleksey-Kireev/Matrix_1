10d0f1744eac8c86014a669434cdd464eac9e688
Aleksey-Kireev <kireev_a_a@mail.ru>
2025-01-10 00:22:28 +0900

    Работа с массивами

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        int[][] matrix = new int[10][10];
-        for (int y = 0; y<10; y++) {
-           for (int x = 0; x < 10; x++) {
-                if (y<=x) {
-                    matrix[x][y] = 1;
-                } else {
-                    matrix[x][y] = 0;
-                }
-           }
 
-       }
-        for (int x = 0; x<10; x++) {
-            for (int y = 0; y < 10; y++) {
-                System.out.print (matrix [x] [y] + " ");
-            }
-            System.out.println (" ");
-        }
+
+        System.out.println (" ");
+        System.out.println ("Пробуем создавать каждую работу в разных ветках");
+        System.out.println (" ");
+
 
 
     }
