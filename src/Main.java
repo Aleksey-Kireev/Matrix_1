5b56d547695fcd40888ae56eac19b5232bc91faa
Aleksey-Kireev <kireev_a_a@mail.ru>
2025-01-10 00:20:38 +0900

    Работа с массивами

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 
+// import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+
+//  ----- =====  Заполнение массива 1 и 0 по возрастанию   ===== -----
+
         int[][] matrix = new int[10][10];
         for (int y = 0; y<10; y++) {
            for (int x = 0; x < 10; x++) {
@@ -20,6 +24,35 @@ public class Main {
             System.out.println (" ");
         }
 
+//  ----- =====  Заполнение массива рандомно от 0 до 100   ===== -----
 
+    System.out.println (" ");
+    System.out.println (" ");
+
+        int[][] matrixRND = new int[10][10];
+        for (int y = 0; y<10; y++) {
+            for (int x = 0; x < 10; x++) {
+                matrixRND [x][y] = (int) (Math.random() * 100);
+            }
+        }
+
+        for (int x = 0; x<10; x++) {
+            for (int y = 0; y < 10; y++) {
+                System.out.print (matrixRND [x] [y] + " ");
+            }
+            System.out.println (" ");
+        }
+
+//  ----- =====  Сортировка массива   ===== -----
+ /*       System.out.println (" ");
+        Arrays.sort(matrixRND);
+        System.out.println ("Отсортированный массив");
+        for (int x = 0; x<10; x++) {
+            for (int y = 0; y < 10; y++) {
+                System.out.print (matrixRND [x] [y] + " ");
+            }
+            System.out.println (" ");
+        }
+*/
     }
 }
